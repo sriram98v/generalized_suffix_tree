83f97d0b0ad7c9ffd1c939c69ae58e62fb711195
sriram98v <vijendran.sriram@gmail.com>
2023-09-14 00:13:14 -0500

    Added test forset of truncated strings

--- tests/tests.rs
+++ tests/tests.rs
@@ -89,16 +89,16 @@ fn add_string_repeats_trunc(){
     assert_eq!(sstring, HashMap::from([(item_id.clone(), HashSet::from([14]))]));
 }
 
-// #[test]
-// fn add_string_set_trunc(){
-//     let mut tree: KGST<char, String> = KGST::new('$');
-//     let string_set: Vec<Vec<char>> = vec!["GATTACAGATTACAXYZGATTACAGATTACA".to_string().chars().collect(), "XYZTTATAGCXYZCGTACAGACCGAA".to_string().chars().collect()];
-//     let id_set:Vec<String> = vec!["first".to_string(),"second".to_string()];
-//     let it = string_set.iter().zip(id_set.iter());
-//     let max_depth: usize = 3;
-//     for (string,id) in it{
-//         tree.add_string(string.clone(), id.clone(), max_depth);
-//     }
-//     let sstring: HashMap<String, HashSet<usize>> = tree.find(&"XYZ".chars().collect());
-//     assert_eq!(sstring, HashMap::from([("first".to_string(), HashSet::from([14])), ("second".to_string(), HashSet::from([0, 10]))]));
-// }
+#[test]
+fn add_string_set_trunc(){
+    let mut tree: KGST<char, String> = KGST::new('$');
+    let string_set: Vec<Vec<char>> = vec!["GATTACAGATTACAXYZGATTACAGATTACA".to_string().chars().collect(), "XYZTTATAGCXYZCGTACAGACCGAA".to_string().chars().collect()];
+    let id_set:Vec<String> = vec!["first".to_string(),"second".to_string()];
+    let it = string_set.iter().zip(id_set.iter());
+    let max_depth: usize = 3;
+    for (string,id) in it{
+        tree.add_string(string.clone(), id.clone(), max_depth);
+    }
+    let sstring: HashMap<String, HashSet<usize>> = tree.find(&"XYZ".chars().collect());
+    assert_eq!(sstring, HashMap::from([("first".to_string(), HashSet::from([14])), ("second".to_string(), HashSet::from([0, 10]))]));
+}
