2fdaf0e4b69a3f02cc0c8a973648cd0aec963e19
sriram98v <sriram.98v@gmail.com>
2023-09-27 13:09:03 -0500

    added docs to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
+//! # K-Truncated Generalized Suffix Tree
+//! Implementation of the [truncated suffix tree](https://pubmed.ncbi.nlm.nih.gov/18283030/)
+//! construction of which is performed in linear time
+//!
+//! ## Examples
+//!
+//! ```
+//! use generalized_suffix_tree::suffix_tree::KGST;
+//! 
+//! // Initalize empty tree
+//! let mut tree: KGST<char, String> = KGST::new('$');
+//! 
+//! // insert item with corresponding item id
+//! let item_string:Vec<char> = "MKAILVVLLYTFTTADADTLCIGYHANNSTDTVDTVLEKNVTVTHSVNLLENRHNGKLCKLRGVAPLHLGKCNIAGWILGNPECESLSTAGSWSYIVETSNPDNGTCYPGDFINYEELREQLSSVSSFEKFEIFPKTSSWPNHDTNRGVTAACPHDGAKSFYRNLLWLVKKEKENSYPMINKSYTNNKGKEVLVLWAIHHPATSADQQSLYQNANAYVFVGSSKYSKKFEPEIAARPKVRDQAGRMKYYWTLVEPGDKITFEATGNLVVPIYAFALKRNSGSGIIISDTSVHDCDTTCQTPNGAINTSLPFQNIHPVTIGECPKYVKSTKLRMATGLRNIPSIQSRGLFGAIAGFIEGGWTGMIDGWYGYHHQNEQGSGYAADLKSTQNAIDGITNKVNSVIEKMNTQFTAVGKEFNHLERRIENLNKKVDDGFLDIWTYNAELLVLLENERTLDYHDSNVKNLYEKVRSQLKNNAKEIGNGCFEFYHKCDDTCMESVKNGTYDYPKYSEEAKLNREEIDGVKLESTRIYQILAIYSTVASSLVLVVSLGAISFWMCSNGSLQCRICI".chars().collect();
+//! let item_id:String = "World".to_string();
+//! tree.insert(item_id.clone(), item_string.clone(),&0);
+//!
+//!
+//! // Query if some string is a substring in the tree
+//! let substring_match = tree.substring_match(&item_string[i..i+max_depth+1]);
+//! 
+//! // Query if some string is a suffix in the tree
+//! let suffix_match = tree.suffix_match(&item_string[i..i+max_depth+1]);
+//!
+//! // Clear tree
+//! tree.clear();
+//! ```
+
 pub mod suffix_node;
 pub mod suffix_tree;
 pub mod tree_item;
