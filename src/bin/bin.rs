7bd922f6149b0b55364e535552992b34464cc8ae
sriram98v <sriram.98v@gmail.com>
2023-10-02 14:58:30 -0500

    added export nodes to bin.rs

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -52,11 +52,39 @@ fn save_tree(tree: &mut KGST<char, String>, output_path: String){
     println!("Saved");
 }
 
+fn save_nodes(tree: &mut KGST<char, String>, output_path: String){
+    println!("Saving tree nodes to {}.", &output_path);
+    let tree_nodes = &tree.export_all_nodes();
+    std::fs::write(
+        output_path,
+        serde_json::to_string_pretty(&tree_nodes).expect("Node export error"),
+    ).unwrap();
+    println!("Saved");
+}
+
 fn main(){
     let matches = Command::new("Generalized suffix tree")
         .version("1.0")
         .author("Sriram Vijendran <vijendran.sriram@gmail.com>")
         .subcommand(Command::new("build")
+            .about("Build suffix tree index from reference fasta file")
+            .arg(arg!(-s --source <SRC_FILE> "Source file with sequences(fasta)")
+                .required(true)
+                )
+            .arg(arg!(-o --out <SAVE_FILE> "save file")
+                .required(true)
+                )
+            .arg(arg!(-d --depth <MAX_DEPTH> "max depth of output tree. (0==length of longest string)")
+                .required(true)
+                .value_parser(clap::value_parser!(usize))
+
+                )
+            .arg(arg!(-n --num <NUM_SEQ> "Number of seq. (0==all)")
+                .required(true)
+                .value_parser(clap::value_parser!(usize))
+                )
+        )
+        .subcommand(Command::new("nodes")
             .about("Build suffix tree index from reference fasta file")
             .arg(arg!(-s --source <SRC_FILE> "Source file with sequences(fasta)")
                 .required(true)
@@ -85,6 +113,14 @@ fn main(){
                     sub_m.get_one::<usize>("depth").expect("required")
                 );
                 save_tree(&mut tree, sub_m.get_one::<String>("out").expect("required").to_string());
+            },
+            Some(("nodes",  sub_m)) => {
+                let mut tree: KGST<char, String> = build_tree(
+                    sub_m.get_one::<String>("source").expect("required").as_str(), 
+                    sub_m.get_one::<usize>("num").expect("required"), 
+                    sub_m.get_one::<usize>("depth").expect("required")
+                );
+                save_nodes(&mut tree, sub_m.get_one::<String>("out").expect("required").to_string());
             },
             _ => {
                 println!("Either build a tree or query an existing tree. Refer help page (-h flag)");
